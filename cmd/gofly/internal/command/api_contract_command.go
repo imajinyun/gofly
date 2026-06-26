b7e1251fa44fd3173c8ddd6263a1ec58d73aa8b3
imajinyun <imajinyun@gmail.com>
2026-06-26 22:16:40 +0800

    refactor(cli): split api import command

--- cmd/gofly/internal/command/api_contract_command.go
+++ cmd/gofly/internal/command/api_contract_command.go
@@ -51,36 +51,6 @@ func splitLeadingNames(args []string) ([]string, []string) {
 	return names, args
 }
 
-func apiImportCommand(args []string) error {
-	leadingSource, args := splitLeadingName(args)
-	fs := flag.NewFlagSet("api import", flag.ContinueOnError)
-	src := fs.String("src", "", "OpenAPI/Swagger JSON or YAML file")
-	from := fs.String("from", "", "OpenAPI/Swagger JSON or YAML file")
-	swagger := fs.String("swagger", "", "Swagger JSON or YAML file, alias for --src")
-	dir := fs.String("dir", ".", "output directory")
-	output := fs.String("output", "", "output .api file")
-	o := fs.String("o", "", "output .api file")
-	service := fs.String("service", "", "service name for generated .api")
-	remaining, err := parseInterspersedFlags(fs, args)
-	if err != nil {
-		return err
-	}
-	if *src == "" {
-		*src = *from
-	}
-	if *src == "" {
-		*src = *swagger
-	}
-	if *src == "" {
-		*src = leadingSource
-	}
-	if *output == "" {
-		*output = *o
-	}
-	fillNameFromArgs(src, remaining)
-	return generator.GenerateAPIFromOpenAPI(generator.APIImportOptions{Source: *src, Dir: *dir, Output: *output, Service: *service})
-}
-
 func apiDiffCommand(args []string) error {
 	leadingFiles, args := splitLeadingNames(args)
 	fs := flag.NewFlagSet("api diff", flag.ContinueOnError)

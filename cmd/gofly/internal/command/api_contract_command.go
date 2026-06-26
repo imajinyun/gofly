abcc29ab6a30266ef84191bd121a15b642c18071
imajinyun <imajinyun@gmail.com>
2026-06-26 22:11:45 +0800

    refactor(cli): split api route command

--- cmd/gofly/internal/command/api_contract_command.go
+++ cmd/gofly/internal/command/api_contract_command.go
@@ -51,32 +51,6 @@ func splitLeadingNames(args []string) ([]string, []string) {
 	return names, args
 }
 
-func apiRouteCommand(args []string) error {
-	leadingFile, args := splitLeadingName(args)
-	fs := flag.NewFlagSet("api route", flag.ContinueOnError)
-	file := fs.String("file", "", "api file")
-	api := fs.String("api", "", "api file")
-	dir := fs.String("dir", ".", "output directory")
-	output := fs.String("output", "", "output routes file")
-	o := fs.String("o", "", "output routes file")
-	format := fs.String("format", "text", "route format: text, markdown, or json")
-	remaining, err := parseInterspersedFlags(fs, args)
-	if err != nil {
-		return err
-	}
-	if *file == "" {
-		*file = *api
-	}
-	if *file == "" {
-		*file = leadingFile
-	}
-	if *output == "" {
-		*output = *o
-	}
-	fillNameFromArgs(file, remaining)
-	return generator.GenerateAPIRoutes(generator.APIRouteOptions{APIFile: *file, Dir: *dir, Output: *output, Format: *format})
-}
-
 func apiImportCommand(args []string) error {
 	leadingSource, args := splitLeadingName(args)
 	fs := flag.NewFlagSet("api import", flag.ContinueOnError)

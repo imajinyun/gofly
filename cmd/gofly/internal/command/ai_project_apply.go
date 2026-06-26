c0c4e1027f6b566191caa1fd44a1b05ccce51ce8
imajinyun <imajinyun@gmail.com>
2026-06-26 20:27:10 +0800

    refactor(cli): split ai project apply dispatch

--- cmd/gofly/internal/command/ai_project_apply.go
+++ cmd/gofly/internal/command/ai_project_apply.go
@@ -81,21 +81,3 @@ func applyAIProjectPlan(plan aiProjectPlan, opts aiProjectApplyOptions) (aiProje
 		MutatesFilesystem: true,
 	}, nil
 }
-
-func runAIProjectApplyCommand(args []string) error {
-	if len(args) < 2 {
-		return fmt.Errorf("%w: scaffold command is incomplete", errUsage)
-	}
-	switch {
-	case args[0] == "new" && args[1] == "service":
-		return serviceNewCommand(args[2:])
-	case args[0] == "new" && args[1] == "api":
-		return apiNewCommand(args[2:])
-	case args[0] == "new" && args[1] == "rpc":
-		return rpcNewCommand(args[2:])
-	case args[0] == "gen" && args[1] == "gateway":
-		return gatewayGenCommand(args[2:])
-	default:
-		return fmt.Errorf("%w: unsupported scaffold command `gofly %s`", errUsage, strings.Join(args, " "))
-	}
-}

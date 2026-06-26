05a1fdc5d551393056e501dc9c15428c8c932e6a
imajinyun <imajinyun@gmail.com>
2026-06-26 21:21:42 +0800

    refactor(cli): split rpc protoc gofly options

--- cmd/gofly/internal/command/rpc_protoc_plugin.go
+++ cmd/gofly/internal/command/rpc_protoc_plugin.go
@@ -6,6 +6,20 @@ import (
 	"strings"
 )
 
+type goflyProtocPluginConfig struct {
+	Dir              string
+	Client           bool
+	Multiple         bool
+	Module           string
+	NameFromFilename bool
+}
+
+type goflyProtocPluginOptions struct {
+	Out     string
+	Options []string
+	Env     []string
+}
+
 func resolveGoflyProtocPlugin(pluginArg string) (string, bool, []string) {
 	plugins := splitCSV(pluginArg)
 	var external []string
@@ -30,6 +44,34 @@ func resolveGoflyProtocPlugin(pluginArg string) (string, bool, []string) {
 		external = append(external, plugin)
 	}
 	return "", false, external
+}
+
+func buildGoflyProtocPluginOptions(enabled bool, config goflyProtocPluginConfig) goflyProtocPluginOptions {
+	options := goflyProtocPluginOptions{
+		Options: []string{"paths=source_relative"},
+	}
+	if !enabled {
+		return options
+	}
+	options.Out = config.Dir
+	options.Env = append(options.Env, "GOFLY_PLUGIN_MODE=protoc")
+	if !config.Client {
+		options.Options = append(options.Options, "no_client=true")
+		options.Env = append(options.Env, "GOFLY_NO_CLIENT=true")
+	}
+	if config.Multiple {
+		options.Options = append(options.Options, "multiple=true")
+		options.Env = append(options.Env, "GOFLY_MULTIPLE=true")
+	}
+	if config.Module != "" {
+		options.Options = append(options.Options, "module="+config.Module)
+		options.Env = append(options.Env, "GOFLY_MODULE="+config.Module)
+	}
+	if config.NameFromFilename {
+		options.Options = append(options.Options, "name_from_filename=true")
+		options.Env = append(options.Env, "GOFLY_NAME_FROM_FILENAME=true")
+	}
+	return options
 }
 
 func isGoflyProtocPluginName(plugin string) bool {

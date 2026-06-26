8436a71f27adcd17c10068f4457df70e3cda5792
imajinyun <imajinyun@gmail.com>
2026-06-26 18:57:02 +0800

    refactor(cli): split model config helpers

--- cmd/gofly/internal/command/model_command.go
+++ cmd/gofly/internal/command/model_command.go
@@ -1,12 +1,9 @@
 package command
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/imajinyun/gofly/cmd/gofly/internal/generator"
 )
@@ -343,39 +340,6 @@ func modelPostgresDatasourceCommand(args []string) error {
 		Cache:         *cache,
 		TypesMap:      typesMap,
 	})
-}
-
-func modelTypesMapFromConfig(configPath, dir string) (map[string]string, error) {
-	path := strings.TrimSpace(configPath)
-	explicitPath := path != ""
-	if path == "" {
-		path = filepath.Join(dir, ".gofly", "config.json")
-	}
-	if _, err := os.Stat(path); err != nil {
-		if errors.Is(err, os.ErrNotExist) && !explicitPath {
-			return nil, nil
-		}
-		return nil, err
-	}
-	cfg, err := generator.LoadConfig(path)
-	if err != nil {
-		return nil, err
-	}
-	if cfg.Model == nil || len(cfg.Model.TypesMap) == 0 {
-		return nil, nil
-	}
-	out := make(map[string]string, len(cfg.Model.TypesMap))
-	for key, value := range cfg.Model.TypesMap {
-		out[key] = value
-	}
-	return out, nil
-}
-
-func registerGoctlModelTemplateFlags(fs *flag.FlagSet) {
-	fs.String("home", "", "template home directory")
-	fs.String("remote", "", "remote template repository")
-	fs.String("branch", "", "remote template branch")
-	fs.Bool("idea", false, "open generated project in IDE")
 }
 
 func modelMongoCommand(args []string) error {
---
+++ cmd/gofly/internal/command/model_config.go
@@ -0,0 +1,44 @@
+package command
+
+import (
+	"errors"
+	"flag"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/imajinyun/gofly/cmd/gofly/internal/generator"
+)
+
+func modelTypesMapFromConfig(configPath, dir string) (map[string]string, error) {
+	path := strings.TrimSpace(configPath)
+	explicitPath := path != ""
+	if path == "" {
+		path = filepath.Join(dir, ".gofly", "config.json")
+	}
+	if _, err := os.Stat(path); err != nil {
+		if errors.Is(err, os.ErrNotExist) && !explicitPath {
+			return nil, nil
+		}
+		return nil, err
+	}
+	cfg, err := generator.LoadConfig(path)
+	if err != nil {
+		return nil, err
+	}
+	if cfg.Model == nil || len(cfg.Model.TypesMap) == 0 {
+		return nil, nil
+	}
+	out := make(map[string]string, len(cfg.Model.TypesMap))
+	for key, value := range cfg.Model.TypesMap {
+		out[key] = value
+	}
+	return out, nil
+}
+
+func registerGoctlModelTemplateFlags(fs *flag.FlagSet) {
+	fs.String("home", "", "template home directory")
+	fs.String("remote", "", "remote template repository")
+	fs.String("branch", "", "remote template branch")
+	fs.Bool("idea", false, "open generated project in IDE")
+}

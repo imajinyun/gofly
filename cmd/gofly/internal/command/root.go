3983da25a802728e34ef2157ab46f5c0c09aa661
imajinyun <imajinyun@gmail.com>
2026-06-26 13:46:23 +0800

    refactor(cli): split completion command

--- cmd/gofly/internal/command/root.go
+++ cmd/gofly/internal/command/root.go
@@ -1314,25 +1314,6 @@ func aiCommand(args []string) error {
 	}
 }
 
-func completionCommand(args []string) error {
-	if printCommandHelp("completion", args) {
-		return nil
-	}
-	if len(args) == 0 {
-		return fmt.Errorf("%w: expected `gofly completion %s`", errUsage, completionShellUsage)
-	}
-	if len(args) > 1 {
-		return fmt.Errorf("%w: completion accepts exactly one shell argument", errUsage)
-	}
-	shell := args[0]
-	script, err := generator.GenerateCompletion(shell)
-	if err != nil {
-		return fmt.Errorf("%w: %v", errUsage, err)
-	}
-	cliOutput(script)
-	return nil
-}
-
 // configCommand 处理 `gofly config init|show|get|set|clean`。
 func configCommand(args []string) error {
 	if printCommandHelp("config", args) {

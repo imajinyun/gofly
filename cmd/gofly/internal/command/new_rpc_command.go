073af7c883a8e55d6d4bae4afd6f16942cd29974
imajinyun <imajinyun@gmail.com>
2026-06-27 11:39:00 +0800

    refactor(cli): reuse finalize helper in new rpc

--- cmd/gofly/internal/command/new_rpc_command.go
+++ cmd/gofly/internal/command/new_rpc_command.go
@@ -58,8 +58,5 @@ func rpcNewCommand(args []string) error {
 	}); err != nil {
 		return err
 	}
-	if err := saveNewScaffoldConfig(*executionFlags.SaveConfig, resolved, cfg); err != nil {
-		return err
-	}
-	return output.printResultWhenRequested(*executionFlags.JSON)
+	return output.finalize(*executionFlags.SaveConfig, cfg, *executionFlags.JSON)
 }

82bb3ebf7545235d2989872dc9b2ca87073b2959
imajinyun <imajinyun@gmail.com>
2026-06-27 14:40:56 +0800

    refactor(cli): normalize template directory source flags

--- cmd/gofly/internal/command/template_source_flags.go
+++ cmd/gofly/internal/command/template_source_flags.go
@@ -8,6 +8,13 @@ type templateSourceFlags struct {
 	Branch *string
 }
 
+type templateDirectorySourceFlags struct {
+	Dir    *string
+	Home   *string
+	Remote *string
+	Branch *string
+}
+
 func registerTemplateSourceFlags(fs *flag.FlagSet, homeUsage, remoteUsage, branchUsage string) templateSourceFlags {
 	if homeUsage == "" {
 		homeUsage = "template home directory"
@@ -22,5 +29,20 @@ func registerTemplateSourceFlags(fs *flag.FlagSet, homeUsage, remoteUsage, branc
 		Home:   fs.String("home", "", homeUsage),
 		Remote: fs.String("remote", "", remoteUsage),
 		Branch: fs.String("branch", "", branchUsage),
+	}
+}
+
+func registerTemplateDirectorySourceFlags(fs *flag.FlagSet) templateDirectorySourceFlags {
+	return templateDirectorySourceFlags{
+		Dir:    fs.String("dir", "", "template output directory"),
+		Home:   fs.String("home", "", "template output directory"),
+		Remote: fs.String("remote", "", "remote template repository or local directory"),
+		Branch: fs.String("branch", "", "remote template branch"),
+	}
+}
+
+func (f templateDirectorySourceFlags) normalize() {
+	if valueFromStringFlag(f.Dir) == "" {
+		setStringFlag(f.Dir, valueFromStringFlag(f.Home))
 	}
 }

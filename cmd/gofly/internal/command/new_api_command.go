549f9843273c8678789cc9fcd2215cdb3e1b23a1
imajinyun <imajinyun@gmail.com>
2026-06-27 09:08:32 +0800

    refactor(cli): split new api result output

--- cmd/gofly/internal/command/new_api_command.go
+++ cmd/gofly/internal/command/new_api_command.go
@@ -127,8 +127,5 @@ func apiNewCommand(args []string) error {
 	if err := saveNewScaffoldConfig(*saveConfig, resolved, cfg); err != nil {
 		return err
 	}
-	if *jsonOut || outputMode() == outputJSON {
-		return output.printResult()
-	}
-	return nil
+	return output.printResultWhenRequested(*jsonOut)
 }
